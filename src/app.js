3893c68e73a3afe7f792b485d78cb9f309c160bc
Martin Møller <mmj@dbc.dk>
2016-09-30 11:35:59 +0200

    Outputting process time in repsonse headers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import koa from 'koa';
 import router from 'koa-router'; // @see https://github.com/alexmingoia/koa-router
+import responseTime from 'koa-response-time';
 import {LoggerMiddleware} from './middlewares/logger.middleware';
 import {log} from './utils/logging';
 
 export function startServer() {
   const app = koa();
   const Router = router();
-  const port = process.env.PORT || 3010; // eslint-disable-line no-process-env
+  const PORT = process.env.PORT || 3010; // eslint-disable-line no-process-env
 
+  app.use(responseTime()); // This middleware should be placed as the very first to ensure that responsetime is correctly calculated
   app.use(LoggerMiddleware());
 
   Router.get('/', function *(next) {
@@ -26,7 +28,7 @@ export function startServer() {
     log.error('Server error', {error: err, ctx: ctx});
   });
 
-  app.listen(port, () => {
-    log.debug(`Server is up and running on port ${port}!`);
+  app.listen(PORT, () => {
+    log.debug(`Server is up and running on port ${PORT}!`);
   });
 }
