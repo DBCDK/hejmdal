7263c4e5093f8def41aa2c41750502e2522a1bd0
Martin Møller <mmj@dbc.dk>
2016-09-30 16:43:58 +0200

    Sandsynligt fix til knækkede CI tests

--- src/middlewares/__tests__/logger.middleware.test.js
+++ src/middlewares/__tests__/logger.middleware.test.js
@@ -2,9 +2,9 @@ import {expect} from 'chai';
 import {LoggerMiddleware} from '../logger.middleware';
 
 
-describe('Testing tests', () => {
+describe('LoggerMiddleware tests', () => {
   it('Should return function', () => {
     const res = LoggerMiddleware();
-    expect(typeof res).to.equal('function');
+    expect(typeof res).to.equal('functions');
   });
 });
