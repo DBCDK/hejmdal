206f9d04c25e7c858adc852139ca0fe640af69b3
Finn Stausgaard <fvs@dbc.dk>
2016-09-30 15:35:10 +0200

    issue #1: Add version and headers (cors and proxy)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
+/**
+ * @file
+ * Configure and start our server
+ */
+
+// Config
+import {version} from '../package.json';
+// path for the API-endpoint, ie /v0/, /v1/, or ..
+const apiPath = '/v' + parseInt(version, 10) + '/';
+
+// Libraries
 import koa from 'koa';
 import router from 'koa-router'; // @see https://github.com/alexmingoia/koa-router
+import cors from 'koa-cors'; // @see https://github.com/evert0n/koa-cors
 import responseTime from 'koa-response-time';
+
+// Middleware
 import {LoggerMiddleware} from './middlewares/logger.middleware';
 import {log} from './utils/logging';
 
@@ -10,6 +24,18 @@ export function startServer() {
   const PORT = process.env.PORT || 3010; // eslint-disable-line no-process-env
 
   app.use(responseTime()); // This middleware should be placed as the very first to ensure that responsetime is correctly calculated
+
+  // Use CORS
+  const corsOptions = {
+    origin: '*',
+    methods: 'GET POST OPTIONS',
+    headers: 'Authorization, Origin, X-Requested-With, Content-Type, Accept'
+  }
+  app.use(cors(corsOptions));
+
+  // trust ip-addresses from X-Forwarded-By header, and log requests
+  app.proxy = true;
+
   app.use(LoggerMiddleware());
 
   Router.get('/', function *(next) {
