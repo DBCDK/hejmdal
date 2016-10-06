63ba13fa0c5ad12565fad12a2c0e6b320afb180a
Martin Møller <mmj@dbc.dk>
2016-10-06 09:57:29 +0200

    #6: optional pretty printing of log (#21)

---
+++ src/utils/logging.util.js
@@ -0,0 +1,93 @@
+import fs from 'fs';
+import {hostname} from 'os';
+import process from 'process';
+import {version} from '../../package.json';
+
+const PRETTY_PRINT = process.env.PRETTY_LOG ? 2 : null;
+
+/**
+ * @returns current log level
+ */
+export function getCurrentLogLevel() {
+  return process.env.LOG_LEVEL || 'INFO'; // eslint-disable-line no-process-env
+}
+
+/**
+ * Convert a log level name to a corresponding numerical value
+ *
+ * @param logLevel log level to convert
+ * @returns numerical log level
+ */
+
+function getNumericalLogLevel(logLevel) {
+  var logLevels = {
+    OFF: 0,
+    ERROR: 1,
+    WARN: 2,
+    WARNING: 2,
+    INFO: 3,
+    DEBUG: 4,
+    TRACE: 5
+  };
+
+  return logLevels[logLevel.toUpperCase()];
+}
+
+/**
+ * Log as JSON to stdout
+ *
+ * @param level log level
+ * @param msg message to log
+ * @param args map of additional key/values to log
+ */
+function doLog(level, msg, args) {
+  const currentNumericalLogLevel = getNumericalLogLevel(getCurrentLogLevel());
+  const targetNumericalLogLevel = getNumericalLogLevel(level);
+
+  if (currentNumericalLogLevel < targetNumericalLogLevel) {
+    return; // level low, do nothing
+  }
+
+  var blob = {
+    '@timestamp': (new Date()).toISOString(),
+    '@version': 1,
+    app: 'hejmdal',
+    version: version,
+    level: level.toUpperCase(),
+    host: hostname(),
+    pid: process.pid,
+    env: process.env.NODE_ENV || 'dev'
+  };
+
+  if (msg) {
+    blob.msg = msg;
+  }
+
+  console.log(JSON.stringify(Object.assign(blob, args), null, PRETTY_PRINT)); // eslint-disable-line no-console
+}
+
+/**
+ * return true if the given path is an existing directory, false
+ * otherwise.
+ */
+export function isDir(path) {
+  try {
+    const stats = fs.lstatSync(path);
+    if (stats.isDirectory()) {
+      return true;
+    }
+  }
+  catch (e) {
+    doLog('warn', 'Failed checking for directory');
+  }
+  return false;
+}
+
+export const log = {
+  log: doLog,
+  info: (msg, args) => doLog('info', msg, args),
+  warn: (msg, args) => doLog('warn', msg, args),
+  error: (msg, args) => doLog('error', msg, args),
+  debug: (msg, args) => doLog('debug', msg, args),
+  trace: (msg, args) => doLog('trace', msg, args)
+};
