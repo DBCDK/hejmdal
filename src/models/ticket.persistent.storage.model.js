cc807c7eeecd16e3ee06da714dbde720dad42099
Finn Stausgaard <fvs@dbc.dk>
2016-10-12 10:16:16 +0200

    #55: use of insert operation throw error

--- src/models/ticket.persistent.storage.model.js
+++ src/models/ticket.persistent.storage.model.js
@@ -30,6 +30,10 @@ export default class PersistenTicketStorage {
       });
   }
 
+  insert(tid, ticket) {   // eslint-disable-line no-unused-vars
+    throw new Error('Cannot use insert in ticket');
+  }
+
   delete(tid) {
     // if (1 == 1) { return true; }   // test - do not delete
     return Ticket.query().delete().where('id', tid)
