70974824d64e7cf045a617356c4cfbd95382d6e6
Finn Stausgaard <fvs@dbc.dk>
2016-10-12 16:24:21 +0200

    #11: move endpoint for getTicket

---
+++ src/routes/getTicket.routes.js
@@ -0,0 +1,11 @@
+import Router from 'koa-router';
+import {VERSION_PREFIX} from '../utils/version.util';
+import {initialize, authenticate} from '../components/identityprovider/identityprovider.component';
+import {getTicket} from '../components/ticket.component';
+import ctxdump from '../components/ctxdump.component.js';
+
+const router = new Router({prefix: VERSION_PREFIX + '/getTicket'});
+
+router.get('/:token/:id', initialize, authenticate, getTicket, ctxdump);
+
+export default router;
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,13 +1,12 @@
 import Router from 'koa-router';
 import {VERSION_PREFIX} from '../utils/version.util';
 import {initialize, authenticate, identityProviderCallback} from '../components/identityprovider/identityprovider.component';
-import {generateTicketData, storeTicket, getTicket} from '../components/ticket.component';
+import {generateTicketData, storeTicket} from '../components/ticket.component';
 import ctxdump from '../components/ctxdump.component.js';
 
 const router = new Router({prefix: VERSION_PREFIX + '/login'});
 
 router.get('/', initialize, authenticate, generateTicketData, storeTicket/* , ctxdump */);
 router.get('/identityProviderCallback/:type/:token', initialize, identityProviderCallback, generateTicketData, storeTicket, ctxdump);
-router.get('/getTicket/:token/:id', initialize, authenticate, getTicket, ctxdump);
 
 export default router;
