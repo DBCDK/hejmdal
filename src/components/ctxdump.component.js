d113843e85ae8283b2b39e19b502aca00d776bd7
Finn Stausgaard <fvs@dbc.dk>
2016-10-18 16:04:02 +0200

    dump ctx

--- src/components/ctxdump.component.js
+++ src/components/ctxdump.component.js
@@ -7,6 +7,10 @@
  */
 export default function ctxdump(ctx, next) {
   const body = ctx.body ? ctx.body : '';
-  ctx.body = body + '<pre>' + JSON.stringify(ctx.session.state, null, 2) + '</pre>';   // For test
+  ctx.body = body + '<pre>' +
+    'ctx:\n' + JSON.stringify(ctx, null, 2) +
+    '\nsession:\n' + JSON.stringify(ctx.session, null, 2) +
+    '\nticket:\n' + JSON.stringify(ctx.ticket, null, 2) +
+    '</pre>';   // For test
   return next();
 }
