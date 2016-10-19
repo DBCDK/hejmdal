6309762225a77bf22970a346f71c3a4c88d2f700
Søren Vibjerg <vibjerg@gmail.com>
2016-10-19 14:15:16 +0200

    Added tests of state middleware

---
+++ src/middlewares/__tests__/state.middleware.test.js
@@ -0,0 +1,62 @@
+import {assert} from 'chai';
+import {stateMiddleware, setDefaultState} from '../state.middleware';
+
+describe('stateMiddleware tests', () => {
+  it('should add functions to ctx', () => {
+    const ctx = {};
+    stateMiddleware(ctx, () => {});
+    assert.isFunction(ctx.setState);
+    assert.isFunction(ctx.getState);
+    assert.isFunction(ctx.setUser);
+    assert.isFunction(ctx.getUser);
+  });
+
+  it('should update state', () => {
+    const ctx = {
+      session: {}
+    };
+    const newState = {test: 'some value'};
+    stateMiddleware(ctx, () => {});
+    ctx.setState(newState);
+    assert.deepEqual(ctx.session.state, newState);
+    assert.deepEqual(ctx.getState(), newState);
+  });
+
+  it('should only update added attributes', () => {
+    const ctx = {
+      session: {
+        state: {old: 'value'}
+      }
+    };
+    const newState = {test: 'some value'};
+    stateMiddleware(ctx, () => {});
+    ctx.setState(newState);
+    assert.deepEqual(ctx.session.state.old, 'value');
+    assert.deepEqual(ctx.getState().old, 'value');
+    assert.deepEqual(ctx.getState().test, 'some value');
+  });
+
+  it('should update user', () => {
+    const ctx = {
+      session: {}
+    };
+    const newState = {test: 'some value'};
+    stateMiddleware(ctx, () => {});
+    ctx.setUser(newState);
+    assert.deepEqual(ctx.session.user, newState);
+    assert.deepEqual(ctx.getUser(), newState);
+  });
+
+  it('should set a default state', () => {
+    const ctx = {
+      session: {},
+      query: {
+        returnurl: 'some_url'
+      }
+    };
+    setDefaultState(ctx, () => {});
+    assert.deepEqual(ctx.session.user, {});
+    assert.isDefined(ctx.session.state.consents);
+    assert.equal(ctx.session.state.returnUrl, 'some_url');
+  });
+});
