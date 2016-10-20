d2eea849edc3ccd41a455efe89020c91dae90e6e
Finn Stausgaard <fvs@dbc.dk>
2016-10-20 11:38:43 +0200

    #18: Mapper component and use set/get State/User

--- src/components/__tests__/ticket.component.test.js
+++ src/components/__tests__/ticket.component.test.js
@@ -1,11 +1,13 @@
 import {assert} from 'chai';
 
 import {storeTicket, getTicket} from '../ticket.component.js';
+import {mockContext} from '../../utils/test.util';
 
 describe('test store and get ticket', () => {
+  const ctx = mockContext();
   const ticket = {
     attributes: null,
-    identifier: 'foo',
+    id: 'foo',
     token: 'bar'
   };
 
@@ -13,9 +15,9 @@ describe('test store and get ticket', () => {
   };
 
   it('should return false for nonexisting ticket', () => {
-    const ctx = {ticket: ticket};
+    ctx.session.state.ticket = ticket;
     getTicket(ctx, next);
-    assert.isFalse(ctx.ticket.attributes);
+    assert.isFalse(ctx.session.state.ticket.attributes);
   });
 
   const attributes = {someId: '123', SomeInfo: '717171'};
@@ -23,39 +25,33 @@ describe('test store and get ticket', () => {
 
   it('should create a ticket-identifier and -token', () => {
     ticket.attributes = attributes;
-    ticket.identifier = null;
-    const ctx = {ticket: ticket};
+    ticket.id = null;
+    ctx.session.state.ticket = ticket;
     return storeTicket(ctx, next).then(() => {
-      assert.isNumber(ctx.ticket.id);
-      assert.isString(ctx.ticket.token);
+      assert.isNumber(ctx.session.state.ticket.id);
+      assert.isString(ctx.session.state.ticket.token);
       params = {
-        token: ctx.ticket.token,
-        id: ctx.ticket.id
+        token: ctx.session.state.ticket.token,
+        id: ctx.session.state.ticket.id
       };
     });
   });
 
   it('should fetch the ticket', () => {
-    const ctx = {
-      ticket: null,
-      params: params
-    };
+    ctx.session.state.ticket = {};
+    ctx.params = params;
 
     return getTicket(ctx, next).then(() => {
-      assert.isObject(ctx.ticket.attributes);
-      assert.deepEqual(ctx.ticket.attributes, attributes);
+      assert.isObject(ctx.session.state.ticket.attributes);
+      assert.deepEqual(ctx.session.state.ticket.attributes, attributes);
     });
   });
 
   it('should only fetch ticket once', () => {
-    const ctx = {
-      session: {
-        state: {}
-      },
-      params: params
-    };
+    ctx.session.state.ticket = {};
+    ctx.params = params;
     return getTicket(ctx, next).then(() => {
-      assert.isFalse(ctx.ticket.attributes);
+      assert.isFalse(ctx.session.state.ticket.attributes);
     });
   });
 });
