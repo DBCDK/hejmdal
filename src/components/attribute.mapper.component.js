7cf6083cc9708a8385708e176e52e18cc619ab4f
Finn Stausgaard <fvs@dbc.dk>
2016-10-25 10:10:38 +0200

    #113: generate birthDate, birthYear and gender (f or m)

--- src/components/attribute.mapper.component.js
+++ src/components/attribute.mapper.component.js
@@ -5,6 +5,8 @@
  *
  */
 
+import {log} from '../utils/logging.util';
+
 /**
  * Attribute mapper
  *
@@ -39,11 +41,13 @@ function mapCulrResponse(culr, fields) {
 
   let cpr = null;
   let libraries = [];
+  let fromCpr = {};
 
   if (culr.accounts && Array.isArray(culr.accounts)) {
     culr.accounts.forEach((account) => {
       if (account.userIdType === 'CPR' && !cpr) {
         cpr = account.userIdValue;
+        fromCpr = mapFromCpr(cpr);
       }
 
       libraries.push({
@@ -53,17 +57,48 @@ function mapCulrResponse(culr, fields) {
     });
   }
 
-  if (fields.includes('cpr')) {
-    mapped.cpr = cpr;
-  }
-
-  if (fields.includes('libraries')) {
-    mapped.libraries = libraries;
-  }
-
-  if (fields.includes('municipality')) {
-    mapped.municipality = culr.municipalityNumber || null;
-  }
+  fields.forEach((field) => {
+    switch (field) {
+      case 'birthDate':
+      case 'birthYear':
+      case 'gender':
+        mapped[field] = fromCpr[field] || null;
+        break;
+      case 'cpr':
+        mapped.cpr = cpr;
+        break;
+      case 'libraries':
+        mapped.libraries = libraries;
+        break;
+      case 'municipality':
+        mapped.municipality = culr.municipalityNumber || null;
+        break;
+      default:
+        log.error('Cannot map attribute: ' + field);
+        break;
+    }
+  });
 
   return mapped;
 }
+
+function mapFromCpr(cpr) {
+  const ret = {};
+  if (isNumeric(cpr) && isValidDate(cpr)) {
+    ret.birthDate = cpr.substr(0, 6);
+    ret.birthYear = cpr.substr(4, 2);
+    ret.gender = cpr.substr(9, 1) % 2 ? 'm' : 'f';
+  }
+  return ret;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function isValidDate(ddmmyy) {
+  const mm = ddmmyy.substr(2, 2) || '00';
+  const d = new Date(ddmmyy.substr(4, 2), mm - 1, ddmmyy.substr(0, 2));
+  return d && (d.getMonth() + 1) === parseInt(mm, 10);
+}
+
