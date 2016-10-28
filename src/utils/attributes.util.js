628c438f1edeea22092c70167f91e202dd13ac42
Finn Stausgaard <fvs@dbc.dk>
2016-10-28 12:15:12 +0200

    #88: smaug ATTRIBUTES description

---
+++ src/utils/attributes.util.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ *
+ * Describe the attributes supported by hejmdal.
+ *
+ * Each service specified in Smaug, can ask for one or more of these attributes.
+ * The values of the attributes are fetched from CULR
+ *
+ */
+
+export const ATTRIBUTES = {
+  cpr: {
+    name: 'CPR-nummer',
+    description: 'Brugerens CPR-nummer'
+  },
+  birthDate: {
+    name: 'Fødselsdato',
+    description: 'Fødselsdato - 6 første cifre af CPR-nummer'
+  },
+  birthYear: {
+    name: 'Fødselsår',
+    description: '4 cifret fødselsår - taget fra CPR-nummer'
+  },
+  gender: {
+    name: 'Køn',
+    description: 'Brugerens køn, m (for male) eller f (for female)'
+  },
+  libraries: {
+    name: 'Biblioteker',
+    description: 'En liste over de biblioteker som kender brugeren'
+  },
+  municipality: {
+    name: 'Kommunenummer',
+    description: '3 cifret kommunenummer'    // http://www.linking.dk/lokalportaler/kommuner.html
+  }
+};
