13edd5aafed8dbc8cc4e4638b4ae348cf501fc17
Martin Møller <mmj@dbc.dk>
2016-10-28 16:39:53 +0200

    ES6

--- selenium/tests/identityProvider.test.js
+++ selenium/tests/identityProvider.test.js
@@ -3,17 +3,17 @@ const assert = require('chai').assert;
 
 import LoginPage from '../pageObjects/loginPage';
 
-describe('Test Smaug tokens', function () {
+describe('Test Smaug tokens', () => {
   const loginPage = new LoginPage();
 
-  it('should login user with UNIlogin', function () {
+  it('should login user with UNIlogin', () => {
     loginPage.loginWithUNIlogin();
     const session = browser.getSession();
+    console.log(session.state);
     assert.deepEqual(session.user, {
       userId: '5555666677',
       userType: 'unilogin',
       identityProviders: ['unilogin']
     });
   });
-
 });
