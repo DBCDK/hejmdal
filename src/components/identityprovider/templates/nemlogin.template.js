9a6ce9d81c05dd896c03eb08e65d5da9570edd82
Finn Stausgaard <fvs@dbc.dk>
2016-10-31 14:36:19 +0100

    #62 - add version and token to test redirect url

--- src/components/identityprovider/templates/nemlogin.template.js
+++ src/components/identityprovider/templates/nemlogin.template.js
@@ -3,6 +3,6 @@
  *
  */
 
-export default () => `
-<a href="/login/identityProviderCallback/nemlogin/qwerty?eduPersonTargetedID=WAYF-DK-16028a572f83fd83cb0728aab8a6cc0685933a04">NEM-login</a>
+export default (version, token) => `
+<a href="${version}/login/identityProviderCallback/nemlogin/${token}?eduPersonTargetedID=WAYF-DK-16028a572f83fd83cb0728aab8a6cc0685933a04">NEM-login</a>
 `;
