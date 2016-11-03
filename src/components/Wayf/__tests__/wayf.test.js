4b7492d35ba3a0d920a6b058969db85b04bf0bde
Finn Stausgaard <fvs@dbc.dk>
2016-11-03 11:26:19 +0100

    #125 - add wayfId to attributes

--- src/components/Wayf/__tests__/wayf.test.js
+++ src/components/Wayf/__tests__/wayf.test.js
@@ -5,8 +5,12 @@ import {mockContext} from '../../../utils/test.util';
 describe('Test wayf component', () => {
   const ctx = mockContext();
 
-  it('Lookup a known user', async () => {
-    assert.deepEqual(await getWayfResponse(ctx), {userId: '0102030405', wayfId: 'WAYF-DK-some-long-md5-like-string'});
+  it('Lookup a known user', async() => {
+    const expected = {
+      userId: '0102030405',
+      wayfId: 'WAYF-DK-some-long-md5-like-string'
+    };
+    assert.deepEqual(await getWayfResponse(ctx), expected);
   });
 
 });
