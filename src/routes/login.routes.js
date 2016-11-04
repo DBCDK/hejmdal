0721ab9c50f90762f17d5cfbc90b9b7e58299d68
Finn Stausgaard <fvs@dbc.dk>
2016-11-04 12:20:56 +0100

    #125 - use koa-compose

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+import compose from 'koa-compose';
 import {VERSION_PREFIX} from '../utils/version.util';
 import {setDefaultState} from '../middlewares/state.middleware';
 import {getAttributes} from '../components/Smaug/smaug.component';
@@ -10,10 +11,17 @@ import * as Culr from '../components/Culr/culr.component';
 
 const router = new Router({prefix: VERSION_PREFIX + '/login'});
 
-router.get('/', setDefaultState, getAttributes, authenticate, Consent.retrieveUserConsent, mapAttributesToTicket, storeTicket);
-router.get('/identityProviderCallback/:type/:token', identityProviderCallback, Consent.retrieveUserConsent, Culr.getCulrAttributes, mapAttributesToTicket, storeTicket);
-router.post('/identityProviderCallback/:type/:token', identityProviderCallback, Consent.retrieveUserConsent, Culr.getCulrAttributes, mapAttributesToTicket, storeTicket);
+const identityProviderCallbackRoute = async (ctx, next) => {
+  await compose([identityProviderCallback, Consent.retrieveUserConsent, culrTicketRoute])(ctx, next);
+};
+const culrTicketRoute = async (ctx, next) => {
+  await compose([Culr.getCulrAttributes, mapAttributesToTicket, storeTicket])(ctx, next);
+};
+
+router.get('/', setDefaultState, getAttributes, authenticate, Consent.retrieveUserConsent, culrTicketRoute);
+router.get('/identityProviderCallback/:type/:token', identityProviderCallbackRoute);
+router.post('/identityProviderCallback/:type/:token', identityProviderCallbackRoute);
 router.get('/consent', Consent.giveConsentUI);
-router.post('/consentsubmit', Consent.consentSubmit, Culr.getCulrAttributes, mapAttributesToTicket, storeTicket);
+router.post('/consentsubmit', Consent.consentSubmit, culrTicketRoute);
 
 export default router;
