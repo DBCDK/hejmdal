90c5137660d51cb5a14240532a4a174fd8e25e40
Martin Møller <mmj@dbc.dk>
2016-11-08 10:43:59 +0100

    Specigicering af platform på saucelabs

--- selenium/config/wdio.conf.js
+++ selenium/config/wdio.conf.js
@@ -49,6 +49,8 @@ let config = {
     // 5 instance gets started at a time.
     maxInstances: 5,
     browserName: 'chrome',
+    platform: 'OS X 10.11',
+    version: 'latest',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     build: process.env.TRAVIS_BUILD_NUMBER
   }],
