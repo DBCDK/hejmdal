8b8c0e03d549a5d0edec33103d9758ef1829d69e
Martin Møller <mmj@dbc.dk>
2016-11-23 15:15:30 +0100

    Added keyboard navigation

--- static/js/borchk.js
+++ static/js/borchk.js
@@ -3,11 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var librariesDropdown = document.getElementById('libraries-dropdown-container');
   var allAgencies = document.getElementsByClassName('agency');
   var currentSeacrhValue = libraryInput.value;
+  var currentlyVisibleAgencies = [];
+  var currentlySelectedIndex = -1;
 
   libraryInput.addEventListener('keyup', function() {
-    currentSeacrhValue = libraryInput.value;
-    toggleBorchkDropdown();
-    toggleVisibles();
+    if (libraryInput.value !== currentSeacrhValue) {
+      currentSeacrhValue = libraryInput.value;
+      toggleBorchkDropdown();
+      toggleVisibles();
+    }
   });
 
   function toggleBorchkDropdown() {
@@ -22,11 +26,56 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function toggleVisibles() {
+    currentlyVisibleAgencies = [];
+
     for (var i = 0; i < allAgencies.length; i++) {
       var item = allAgencies.item(i);
-      item.classList.toggle('hide', !allAgencies.item(i).textContent.toLowerCase().includes(currentSeacrhValue.toLowerCase()));
+      item.classList.remove('selected');
+      var shouldHide = !allAgencies.item(i).textContent.toLowerCase().includes(currentSeacrhValue.toLowerCase());
+      item.classList.toggle('hide', shouldHide);
+
+      if (!shouldHide) {
+        currentlyVisibleAgencies.push(item);
+        currentlySelectedIndex = -1;
+      }
     }
   }
+
+  $(window).keydown(function(e) { // eslint-disable-line no-undef
+    if (e.which !== 40 && e.which !== 38) {
+      return;
+    }
+
+    e.preventDefault();
+    if (e.which === 40) {
+      if (currentlySelectedIndex >= 0) {
+        currentlyVisibleAgencies[currentlySelectedIndex].classList.remove('selected');
+        currentlySelectedIndex++;
+
+        if (currentlySelectedIndex >= currentlyVisibleAgencies.length) {
+          currentlySelectedIndex = 0;
+        }
+      }
+      else {
+        currentlySelectedIndex = 0;
+      }
+      currentlyVisibleAgencies[currentlySelectedIndex].classList.add('selected');
+    }
+    else if (e.which === 38) {
+      if (currentlySelectedIndex >= 0) {
+        currentlyVisibleAgencies[currentlySelectedIndex].classList.remove('selected');
+        currentlySelectedIndex--;
+
+        if (currentlySelectedIndex <= -1) {
+          currentlySelectedIndex = currentlyVisibleAgencies.length - 1;
+        }
+      }
+      else {
+        currentlySelectedIndex = currentlyVisibleAgencies.length - 1;
+      }
+      currentlyVisibleAgencies[currentlySelectedIndex].classList.add('selected');
+    }
+  });
 });
 
 /**
