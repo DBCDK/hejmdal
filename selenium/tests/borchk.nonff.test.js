f79b5bdfa17a3151329a29ae2d112f4ede9fe3a7
Martin Møller <mmj@dbc.dk>
2016-11-25 16:25:08 +0100

    Changed keys to unicode

--- selenium/tests/borchk.nonff.test.js
+++ selenium/tests/borchk.nonff.test.js
@@ -19,13 +19,13 @@ describe('Test Borchk component', () => {
     browser.addValue('#libraryid-input', 'rin');
 
     // Pressing ArrowDown key -- https://github.com/webdriverio/webdriverio/blob/master/lib/helpers/constants.js#L67
-    browser.keys('ArrowDown');
+    browser.keys('\uE015');
 
     // Get text of currently selected item in dropdrown
     const selectedElementText = browser.element('.selected').getText();
 
     // Pressing Tab key
-    browser.keys('Tab');
+    browser.keys('\uE004');
 
     // Get value of library input field
     const inputValue = browser.element('#libraryid-input').getValue();
@@ -40,13 +40,13 @@ describe('Test Borchk component', () => {
     browser.addValue('#libraryid-input', 'rin');
 
     // Pressing ArrowDown key -- https://github.com/webdriverio/webdriverio/blob/master/lib/helpers/constants.js#L67
-    browser.keys('ArrowDown');
+    browser.keys('\uE015');
 
     // Get text of currently selected item in dropdrown
     const selectedElementText = browser.element('.selected').getText();
 
     // Pressing Enter key
-    browser.keys('Enter');
+    browser.keys('\uE007');
 
     // Get value of library input field
     const inputValue = browser.element('#libraryid-input').getValue();
@@ -62,7 +62,7 @@ describe('Test Borchk component', () => {
     browser.addValue('#libraryid-input', 'rin');
     assert.isTrue(browser.isVisible('#libraries-dropdown'));
     // Pressing escape key -- https://github.com/webdriverio/webdriverio/blob/master/lib/helpers/constants.js#L67
-    browser.keys('Escape');
+    browser.keys('\uE00C');
     assert.isFalse(browser.isVisible('#libraries-dropdown'));
   });
 
@@ -80,7 +80,7 @@ describe('Test Borchk component', () => {
 
 
     // Select first item in dropdown -- ArrowDown Enter
-    browser.keys(['ArrowDown', 'Enter']);
+    browser.keys(['\uE015', '\uE007']);
 
     // We should now have one item with the .recent class set
     assert.equal(browser.elements('.recent').value.length, 1);
@@ -93,7 +93,7 @@ describe('Test Borchk component', () => {
     assert.isTrue(browser.isVisible('#alphabetical'));
 
     // ArrowDown ArrowDown ArrowDown Enter
-    browser.keys(['ArrowDown', 'ArrowDown', 'ArrowDown', 'Enter']);
+    browser.keys(['\uE015', '\uE015', '\uE015', '\uE007']);
 
     browser.setValue('#libraryid-input', '');
     browser.addValue('#libraryid-input', 'rin');
