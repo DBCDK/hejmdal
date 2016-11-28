f5f3ba70d19cb6319bccc418168a7529998da0ee
Martin Møller <mmj@dbc.dk>
2016-11-28 14:04:26 +0100

    Testing on chrome/OSX only

--- selenium/config/sauce.conf.js
+++ selenium/config/sauce.conf.js
@@ -8,36 +8,6 @@ export default {
       version: 'latest',
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER
-    },
-    {
-      maxInstances: 1,
-      browserName: 'chrome',
-      platform: 'Windows 10',
-      version: 'latest',
-      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-      build: process.env.TRAVIS_BUILD_NUMBER
-    },
-    {
-      maxInstances: 1,
-      browserName: 'firefox',
-      platform: 'OS X 10.11',
-      version: '47.0',
-      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-      build: process.env.TRAVIS_BUILD_NUMBER,
-      exclude: [
-        './selenium/**/*.nonff.test.js' // excluded due to missing support for Storage and keys in Firefox selenium driver
-      ]
-    },
-    {
-      maxInstances: 1,
-      browserName: 'firefox',
-      platform: 'Windows 10',
-      version: '47.0',
-      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-      build: process.env.TRAVIS_BUILD_NUMBER,
-      exclude: [
-        './selenium/**/*.nonff.test.js' // excluded due to missing support for Storage and keys in Firefox selenium driver
-      ]
     }
   ]
 };
