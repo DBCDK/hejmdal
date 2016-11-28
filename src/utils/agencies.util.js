3606f9e43dd1c8ff015495cedf141a7daf2950f3
Finn Stausgaard <fvs@dbc.dk>
2016-11-28 10:17:36 +0100

    #135: refactor

--- src/utils/agencies.util.js
+++ src/utils/agencies.util.js
@@ -23,15 +23,45 @@ export async function cacheAgencies() {
  * @return {Array}
  */
 export async function getListOfAgenciesForFrontend() {
+  await setUiList();
+
+  return uiList;
+}
+
+/**
+ * Return the name of the agency if found in agency list
+ *
+ * @param agencyId
+ * @param agencyList
+ * @returns {*}
+ */
+export async function getAgencyName(agencyId) {
+  await setAgencyList();
+
+  let name = '';
+  if (agencyId) {
+    name = 'Ukendt bibliotek: ' + agencyId;
+    AgencyList.forEach((agency) => {
+      if (agency.branchId === agencyId) {
+        name = agency.name;
+      }
+    });
+  }
+  return name;
+}
+
+async function setAgencyList() {
   if (!AgencyList || !AgencyList.length) {
     await cacheAgencies();
   }
+}
+
+async function setUiList() {
+  await setAgencyList();
 
   if (!uiList || !uiList.length) {
     uiList = AgencyList.map((agency) => {
       return {branchId: agency.branchId, name: agency.name};
     });
   }
-
-  return uiList;
 }
