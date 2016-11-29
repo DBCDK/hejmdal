c6a9da67e4f9401af6f7f4a8b8af9a25599583b4
Finn Stausgaard <fvs@dbc.dk>
2016-11-29 16:26:24 +0100

    #158: logout component

--- src/routes/logout.routes.js
+++ src/routes/logout.routes.js
@@ -4,25 +4,10 @@
  */
 import Router from 'koa-router'; // @see https://github.com/alexmingoia/koa-router
 import {VERSION_PREFIX} from '../utils/version.util';
+import {logout} from '../components/Logout/logout.component';
 
 const router = new Router({prefix: VERSION_PREFIX});
 
-/**
- * Sets the session to null which will provoke the session to be destroyed.
- * After the session is destroyed a message is shown to the user, unless a redirect paramter is present. If
- * that's the case the browser will be redirected to that.
- */
-router.get('/logud', async(ctx, next) => {
-  ctx.session = null;
-
-  if (ctx.request.query.redirect) {
-    ctx.redirect(ctx.request.query.redirect);
-  }
-  else {
-    ctx.body = 'Du er nu logget ud';
-  }
-
-  await next();
-});
+router.get('/logout', logout);
 
 export default router;
