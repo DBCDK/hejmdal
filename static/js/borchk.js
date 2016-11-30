7b046b48384d5962681dfa5c0caf7d4ff6f86216
Martin Møller <mmj@dbc.dk>
2016-11-30 11:38:21 +0100

    Fixe bithound check

--- static/js/borchk.js
+++ static/js/borchk.js
@@ -44,15 +44,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   if (document.getElementById('error-overlay')) {
-    document.getElementById('error-overlay-close-btn').addEventListener('click', function(e) {
-      e.preventDefault();
-      closeErrorOverlay();
-    });
+    document.getElementById('error-overlay-close-btn').addEventListener('click', closeErrorOverlay);
 
-    document.getElementById('try-again-btn').addEventListener('click', function(e) {
-      e.preventDefault();
-      closeErrorOverlay();
-    });
+    document.getElementById('try-again-btn').addEventListener('click', closeErrorOverlay);
   }
 
   if (Storage !== undefined) { // eslint-disable-line no-undefined
@@ -324,7 +318,14 @@ function removeExistingRecentlySelectedLibraries() {
   });
 }
 
-function closeErrorOverlay() {
+/**
+ * Closes the eroroverlay
+ * @param {Event} e
+ */
+function closeErrorOverlay(e) {
+  if(e){
+    e.preventDefault();
+  }
   document.getElementById('topbar-container').setAttribute('aria-hidden', 'false');
   document.getElementById('content-container').setAttribute('aria-hidden', 'false');
   document.getElementById('footer-container').setAttribute('aria-hidden', 'false');
