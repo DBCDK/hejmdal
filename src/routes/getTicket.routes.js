653bfe971eb86256d8e13ef508061051a15ca479
Søren Vibjerg <vibjerg@gmail.com>
2016-12-14 14:07:56 +0100

    remove authenticate middleware from ticket. Requesting server does not have a valid session

--- src/routes/getTicket.routes.js
+++ src/routes/getTicket.routes.js
@@ -1,10 +1,9 @@
 import Router from 'koa-router';
 import {VERSION_PREFIX} from '../utils/version.util';
-import {authenticate} from '../components/Identityprovider/identityprovider.component';
 import {getTicket} from '../components/Ticket/ticket.component';
 
 const router = new Router({prefix: VERSION_PREFIX + '/getTicket'});
 
-router.get('/:token/:id', authenticate, getTicket);
+router.get('/:token/:id', getTicket);
 
 export default router;
