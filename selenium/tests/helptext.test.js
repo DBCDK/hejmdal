a7508eb8393d7cee89724c08d41e94bafe697d80
Martin Møller <mmj@dbc.dk>
2016-12-14 13:02:34 +0100

    Testing the helptext link on loginpage and error overlay

---
+++ selenium/tests/helptext.test.js
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Testing the helptext functionality
+ */
+
+import {assert} from 'chai';
+import LoginPage from '../pageObjects/loginPage';
+
+describe('Testing the helptext functionality', () => {
+  let loginpage;
+
+  beforeEach(() => {
+    loginpage = new LoginPage();
+  });
+
+  it('Assert true is true', () => {
+    assert.isFalse(browser.isVisible('#helpModal'));
+    browser.click('#helptext-link');
+    browser.waitForVisible('#helpModal', 600);
+    assert.isTrue(browser.isVisible('#helpModal'));
+  });
+
+  it('Assert true is true', () => {
+    loginpage.open({token: loginpage.validToken, error: 'unknown_error'});
+    assert.isFalse(browser.isVisible('#helpModal'));
+    browser.element('#error-overlay').click('#helptext-link');
+    browser.waitForVisible('#helpModal', 600);
+    assert.isTrue(browser.isVisible('#helpModal'));
+  });
+});
