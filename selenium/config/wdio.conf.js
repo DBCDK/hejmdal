e5f4c012f2e4c80d32dce996433fbbe7ff8a711c
Søren Vibjerg <vibjerg@gmail.com>
2016-12-15 09:33:40 +0100

    Let travis control sauce connect

--- selenium/config/wdio.conf.js
+++ selenium/config/wdio.conf.js
@@ -113,7 +113,6 @@ let config = {
   services: ['sauce'],
   user: process.env.SAUCE_USERNAME,
   key: process.env.SAUCE_ACCESS_KEY,
-  sauceConnect: process.env.CI && false || true,
 
   //
   // Framework you want to run your specs with.
