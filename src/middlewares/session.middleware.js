d83f6d3a431b631c37f90a2f76d94e416175aa9c
Søren Vibjerg <vibjerg@gmail.com>
2016-12-21 14:12:28 +0100

    Copy session method from koa-session2 to add remember me functionality

--- src/middlewares/session.middleware.js
+++ src/middlewares/session.middleware.js
@@ -4,18 +4,56 @@
  */
 
 import {CONFIG} from '../utils/config.util';
+import {Store} from 'koa-session2';
+
 
 /**
- * Checks if session is set and refreshes time of expiry.
- * If sesseion is unset an empty object will be set.
+ * Handles sessions.
  *
- * @param {object} ctx
- * @param {function} next
+ * Is a modification of koa-session2, to support remember-me functionality
+ * @see https://github.com/Secbone/koa-session2/blob/master/index.js
+ *
+ * @param opts
+ * @returns {Function}
  */
-export async function SessionMiddleware(ctx, next) {
-  if (Object.keys(ctx.session).length === 0) {
-    ctx.session = {};
-  }
+export default function session(opts = {}) {
+  opts.key = opts.key || 'koa:sess';
+  opts.store = opts.store || new Store();
 
-  await next();
+  return async function (ctx, next) {
+    let id = ctx.cookies.get(opts.key, opts);
+
+    if (!id) {
+      ctx.session = {};
+    }
+    else {
+      ctx.session = await opts.store.get(id);
+      // check session should be a no-null object
+      if (typeof ctx.session !== 'object' || ctx.session === null) {
+        ctx.session = {};
+      }
+    }
+
+    let old = JSON.stringify(ctx.session);
+
+    await next();
+
+    // if not changed
+    if (old === JSON.stringify(ctx.session)) {
+      return;
+    }
+
+    // clear old session if exists
+    if (id) {
+      await opts.store.destroy(id);
+      id = null;
+    }
+
+    // set new session
+    if (ctx.session && Object.keys(ctx.session).length) {
+      let sid = await opts.store.set(ctx.session, Object.assign({}, opts, {sid: id}));
+      const maxAge = ctx.session.rememberMe && CONFIG.session.life_time || null;
+      ctx.cookies.set(opts.key, sid, Object.assign({}, opts, {maxAge}));
+    }
+  };
 }
