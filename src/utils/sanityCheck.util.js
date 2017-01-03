1a750ac9fce12aa74e5505930148886f5e941c42
Søren Vibjerg <vibjerg@gmail.com>
2017-01-03 14:30:41 +0100

    Borchk sanitycheck

--- src/utils/sanityCheck.util.js
+++ src/utils/sanityCheck.util.js
@@ -4,15 +4,35 @@
  */
 
 import Session from '../models/db_models/session.model';
+import * as Borchk from './components/Borchk/borchk.client';
 import {log} from './logging.util';
 
 export default function sanityCheck() {
   checkDatabase();
+  checkBorchk();
 }
 
+/**
+ * Check if database is available
+ */
 export function checkDatabase() {
   Session.query().count('*')
     .catch((e) => {
       log.error('Query failed', {error: e.message, stack: e.stack});
     });
 }
+
+/**
+ * Check if Borchk webservice is responding
+ */
+export function checkBorchk() {
+  Borchk.getClient('check', 'check', 'check', 'check').then(res => {
+    if (!res.borrowerCheckResponse) {
+      throw Error('No valid response from borchk');
+    }
+  }).catch(e => {
+    log.error('Borchk is failing', {error: e.message, stack: e.stack});
+  });
+
+}
+
