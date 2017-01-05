3a131e49a886f1fb495bcfd9a1587e720e3d509c
Søren Vibjerg <vibjerg@gmail.com>
2017-01-05 09:12:05 +0100

    Log errors in middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -4,6 +4,7 @@
  */
 
 import {VERSION_PREFIX} from '../utils/version.util';
+import {log} from '../utils/logging.util';
 
 /**
  * Adds the get- and setState methods to the context object
@@ -25,17 +26,19 @@ export default async function errorMiddleware(ctx, next) {
   catch (err) {
     ctx.status = err.status || 500;
     const state = ctx.getState();
-
     const link = state && state.smaugToken && {
       href: `${VERSION_PREFIX}/login?token=${state.smaugToken}`,
       value: 'Prøv igen'
     };
     let error = 'Der er sket en fejl. Prøv at logge ind igen';
     if (ctx.status === 403) {
-      error = 'Du har ikke adgang til denne side. Dette kan skyldes at der benyttes et ugyldigt token';
+      error = 'Du har ikke adgang til denne side. Dette kan skyldes, at der benyttes et ugyldigt token';
     }
     else if (ctx.status === 404) {
-      error = 'Siden du forsøger at tilgå findes ikke';
+      error = 'Siden, du forsøger at tilgå, findes ikke';
+    }
+    else {
+      log.error('An error has happened', {error: err.message, stack: err.stack, url: ctx.url});
     }
 
     ctx.render('Error', {error, link});
