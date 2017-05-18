62864a2603d8ff83b1d49b46a6f996896a033bf0
Martin Møller <mmj@dbc.dk>
2017-05-18 10:46:28 +0200

    Removing IE testing

--- selenium/config/sauce.conf.js
+++ selenium/config/sauce.conf.js
@@ -1,19 +1,11 @@
 export default {
   maxInstances: 1,
   capabilities: [
-    /*{
+    {
       maxInstances: 1,
       browserName: 'chrome',
       platform: 'OS X 10.11',
       version: 'latest',
-      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-      build: process.env.TRAVIS_BUILD_NUMBER
-    },*/
-    {
-      maxInstances: 1,
-      browserName: 'internet explorer',
-      platform: 'Windows 7',
-      version: '11.0',
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER
     }
