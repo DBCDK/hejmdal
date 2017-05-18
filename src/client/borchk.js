fdefda1a35c42173f0e25b5ffa6c6a39bba0d5ac
Martin Møller <mmj@dbc.dk>
2017-05-18 10:36:24 +0200

    - added polyfill for classList.toggle - added separate ie-polyfill file

--- src/client/borchk.js
+++ src/client/borchk.js
@@ -11,30 +11,6 @@ var currentSearchValue = '';
 var libraryInput;
 var libraryIdInput;
 
-/*! Created by MDN {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find} */
-if (!Array.prototype.find) {
-  Array.prototype.find = function(predicate) {
-    if (this === null) {
-      throw new TypeError('Array.prototype.find called on null or undefined');
-    }
-    if (typeof predicate !== 'function') {
-      throw new TypeError('predicate must be a function');
-    }
-    var list = Object(this)
-    var length = list.length >>> 0;
-    var thisArg = arguments[1];
-    var value;
-
-    for (var i = 0; i < length; i++) {
-      value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
-    }
-    return undefined;
-  };
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   var body = document.getElementsByTagName('body')[0];
   libraryInput = document.getElementById('libraryname-input');
---
+++ src/client/ie-polyfill.js
@@ -0,0 +1,82 @@
+/*! Created by MDN {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find} */
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this)
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+// Source: https://gist.github.com/k-gun/c2ea7c49edf7b757fe9561ba37cb19ca
+;(function() {
+  // helpers
+  var regExp = function(name) {
+    return new RegExp('(^| )'+ name +'( |$)');
+  };
+  var forEach = function(list, fn, scope) {
+    for (var i = 0; i < list.length; i++) {
+      fn.call(scope, list[i]);
+    }
+  };
+
+  // class list object with basic methods
+  function ClassList(element) {
+    this.element = element;
+  }
+
+  ClassList.prototype = {
+    add: function() {
+      forEach(arguments, function(name) {
+        if (!this.contains(name)) {
+          this.element.className += this.element.className.length > 0 ? ' ' + name : name;
+        }
+      }, this);
+    },
+    remove: function() {
+      forEach(arguments, function(name) {
+        this.element.className =
+          this.element.className.replace(regExp(name), '');
+      }, this);
+    },
+    toggle: function(name) {
+      return this.contains(name)
+        ? (this.remove(name), false) : (this.add(name), true);
+    },
+    contains: function(name) {
+      return regExp(name).test(this.element.className);
+    },
+    // bonus..
+    replace: function(oldName, newName) {
+      this.remove(oldName), this.add(newName);
+    }
+  };
+
+  // IE8/9, Safari
+  if (!('classList' in Element.prototype)) {
+    Object.defineProperty(Element.prototype, 'classList', {
+      get: function() {
+        return new ClassList(this);
+      }
+    });
+  }
+
+  // replace() support for others
+  if (window.DOMTokenList && DOMTokenList.prototype.replace == null) {
+    DOMTokenList.prototype.replace = ClassList.prototype.replace;
+  }
+})();
