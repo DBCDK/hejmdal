d71ce252cde453e02cf41fbc9b38fe9ceb90d45a
Martin Møller <mmj@dbc.dk>
2017-05-18 09:41:40 +0200

    String.includes not supported in IE

--- selenium/config/sauce.conf.js
+++ selenium/config/sauce.conf.js
@@ -8,6 +8,14 @@ export default {
       version: 'latest',
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER
+    },
+    {
+      maxInstances: 1,
+      browserName: 'internet explorer',
+      platform: 'Windows 7',
+      version: '11.0',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_BUILD_NUMBER
     }
   ]
 };
