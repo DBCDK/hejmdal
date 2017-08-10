3a8a3b7a2e7006eb24946dc0869dc3a46f097c34
Finn Stausgaard <fvs@dbc.dk>
2017-08-10 16:31:01 +0200

    #362: Add gatewayfticket table and read data for new redirect scheme

--- src/components/GateWayf/gatewayf.component.js
+++ src/components/GateWayf/gatewayf.component.js
@@ -5,6 +5,8 @@
 import {form} from 'co-body';
 import {CONFIG} from '../../utils/config.util';
 import {log} from '../../utils/logging.util';
+import KeyValueStorage from '../../models/keyvalue.storage.model';
+import PersistentTicketStorage from '../../models/GateWayfTicket/gatewayfticket.persistent.storage.model';
 import {getMockedGateWayfUrl, getMockedGateWayfResponse} from './mock/gatewayf.mock';
 
 /**
@@ -15,26 +17,17 @@ import {getMockedGateWayfUrl, getMockedGateWayfResponse} from './mock/gatewayf.m
  * @returns {{userId: *}}
  */
 export async function getGateWayfResponse(ctx, idp) {
-  let cpr = null;
-  let wayfId = null;
+  let userInfo = {userId: null, wayfId: null};
   try {
-    const match = ':CPR:';
     const wayfObj = CONFIG.mock_externals[idp] ? getMockedGateWayfResponse(idp) : await form(ctx);
-    if (Array.isArray(wayfObj.schacPersonalUniqueID)) {
-      const cprPos = wayfObj.schacPersonalUniqueID[0].indexOf(match);
-      if (cprPos !== -1) {
-        cpr = wayfObj.schacPersonalUniqueID[0].substr(cprPos + match.length);
-      }
-    }
-    if (Array.isArray(wayfObj.eduPersonTargetedID)) {
-      wayfId = wayfObj.eduPersonTargetedID[0];
-    }
+    userInfo = await getWayfDataFromDb(wayfObj);
+    // userInfo = Array.isArray(wayfObj.id) ? await getWayfDataFromDb(wayfObj) : getWayfDataFromRedirect(wayfObj);
   }
   catch (e) {
     log.error('Could not retrieve ' + idp + ' response', {error: e.message, stack: e.stack});
   }
 
-  return {userId: cpr, wayfId: wayfId};
+  return userInfo;
 
 }
 
@@ -47,6 +40,52 @@ export async function getGateWayfResponse(ctx, idp) {
  */
 export function getGateWayfUrl(idp, token) {
   return CONFIG.mock_externals[idp] ? getMockedGateWayfUrl(idp, token) : getLiveGateWayfUrl(idp, token);
+}
+
+/**
+ * Extract user info from the wayf object
+ *
+ * @param wayfObj
+ * @returns {{userId: *, wayfId: *}}
+ */
+function getWayfDataFromRedirect(wayfObj) {
+  let cpr = null;
+  let wayfId = null;
+  if (Array.isArray(wayfObj.schacPersonalUniqueID)) {
+    const match = ':CPR:';
+    const cprPos = wayfObj.schacPersonalUniqueID[0].indexOf(match);
+    if (cprPos !== -1) {
+      cpr = wayfObj.schacPersonalUniqueID[0].substr(cprPos + match.length);
+    }
+  }
+  if (Array.isArray(wayfObj.eduPersonTargetedID)) {
+    wayfId = wayfObj.eduPersonTargetedID[0];
+  }
+  return {userId: cpr, wayfId: wayfId};
+}
+
+/**
+ * Fetch userinfo from gateWayf in database
+ *
+ * @param wayfTicket
+ * @returns {{userId, wayfId}|{userId: *, wayfId: *}}
+ */
+async function getWayfDataFromDb(wayfTicket) {
+  const storage = new KeyValueStorage(new PersistentTicketStorage());
+  let id;
+  let secret;
+  id = 10; secret = 'bdda523df71337adecfc783e87cfd1a62c3031f98a4d0cecb930e99d22bd2ade';
+  if (Array.isArray(wayfTicket.id)) {
+    id = wayfTicket.id[0];
+  }
+  if (Array.isArray(wayfTicket.secret)) {
+    secret = wayfTicket.secret[0];
+  }
+  // console.log('id', id, 'secret', secret);
+  const gateWayfTicket = true ? await storage.read(id, secret): getMockedGateWayfResponse('wayf');  // eslint-disable-line no-constant-condition
+  // console.log('gatewayfticket', gateWayfTicket);
+  // console.log('mock', getMockedGateWayfResponse('wayf'));
+  return getWayfDataFromRedirect(gateWayfTicket);
 }
 
 /**
