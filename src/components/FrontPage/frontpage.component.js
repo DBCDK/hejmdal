44163f231090bad4b2533578c2a2783aee5d6de8
Soeren Vibjerg <vibjerg@gmail.com>
2018-09-25 09:33:40 +0200

    enable frontpage

--- src/components/FrontPage/frontpage.component.js
+++ src/components/FrontPage/frontpage.component.js
@@ -5,8 +5,8 @@
 
 import {getText} from '../../utils/text.util';
 
-export async function renderFrontPage(ctx, next) {
-  await ctx.render('Frontpage', {
+export async function renderFrontPage(req, res, next) {
+  await res.render('Frontpage', {
     help: getText(['deleteConsents'])
   });
   await next();
