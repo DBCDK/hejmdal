efb96cfb1f45e71a26df8f7cf9de9c44f957a4b9
Soeren Vibjerg <vibjerg@gmail.com>
2018-10-01 15:33:54 +0200

    add user model

---
+++ src/models/User/__tests__/user.model.test.js
@@ -0,0 +1,55 @@
+import {assert} from 'chai';
+import sinon from 'sinon';
+import Store from '../user.persistent.storage.model';
+
+import Knex from 'knex';
+import {Model} from 'objection';
+
+// Utils
+import {CONFIG} from '../../../utils/config.util';
+
+// Initialize knex.
+const knex = Knex(CONFIG.postgres);
+Model.knex(knex);
+
+describe('User store tests', () => {
+  let sandbox;
+  let store;
+
+  beforeEach(() => {
+    store = new Store();
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should upsert user', async () => {
+    const user = {
+      userId: '1122334455'
+    };
+    const res = await store.insert(user);
+    assert.isTrue(res > 0);
+  });
+
+  it('should get user', async () => {
+    const user = {
+      userId: '1122334455'
+    };
+    await store.update(user.userId, user);
+    const userFromDb = await store.read(user.userId);
+    assert.deepEqual(user, userFromDb);
+  });
+ 
+  it('should delete user', async () => {
+    const user = {
+      userId: '1122334455'
+    };
+    const res = await store.insert(user);
+    assert.isTrue(res > 0);
+    await store.delete(user.userId);
+    const userFromDb = await store.read(user.userId);
+    assert.isNull(userFromDb);
+  });
+});
