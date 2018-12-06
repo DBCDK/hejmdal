b21246bff54bde1823296a16ecf2504fdae2f532
mtni <mtni@dbc.dk>
2018-12-06 14:04:19 +0100

    red error box fix + modal close on ESC fix

--- src/client/modal.js
+++ src/client/modal.js
@@ -9,16 +9,32 @@ function onDimmerClick() {
   dimmer.classList.remove('visible');
 }
 
-function toggleModal(modal) {
-  // show dimmer
+function toggleModal(modal, status = 'toggle') {
   var dimmer = document.getElementById('dimmer');
-  dimmer.classList.toggle('visible');
-  // show specific modal
   var modal = document.getElementById(modal);
+
+  // force open modal
+  if (status === 'open') {
+    dimmer.classList.add('visible');
+    modal.classList.add('visible');
+    return;
+  }
+
+  // force close modal
+  if (status === 'close') {
+    dimmer.classList.remove('visible');
+    modal.classList.remove('visible');
+    return;
+  }
+
+  // toggle modal
+  dimmer.classList.toggle('visible');
   modal.classList.toggle('visible');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  var body = document.getElementsByTagName('body')[0];
+  var modal = document.getElementsByClassName('modal')[0];
   var modalBody = document.getElementById('modal-body');
   var modalHeader = document.getElementById('modal-header');
 
@@ -28,6 +44,15 @@ document.addEventListener('DOMContentLoaded', function() {
       modalHeader.classList.add('onScroll');
     } else {
       modalHeader.classList.remove('onScroll');
+    }
+  });
+
+  body.addEventListener('keyup', function(e) {
+    // if ESC pressed
+    if (e.keyCode == 27) {
+      if (modal.classList.contains('visible')) {
+        toggleModal(modal.id, 'close');
+      }
     }
   });
 });
