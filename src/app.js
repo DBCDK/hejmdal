c97cd0e5686e04510c1abc98a9a804b7bfd0713b
Søren Vibjerg <vibjerg@gmail.com>
2019-04-24 09:04:03 +0200

    Add migration command when initializing db (#545)
    Add migration command when initializing db

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ const KnexSessionStore = require('connect-session-knex')(session);
 import model from './oAuth2/oAuth2.model';
 import OAuthServer from 'express-oauth-server';
 import initPassport from './oAuth2/passport';
-import Knex from 'knex';
-import {Model} from 'objection';
 import {setHeaders} from './middlewares/headers.middleware';
 import {loggerMiddleware} from './middlewares/logger.middleware';
 import errorMiddleware from './middlewares/error.middleware';
@@ -22,13 +20,6 @@ import {cacheAgencies} from './utils/agencies.util';
 
 // Utils
 import {CONFIG} from './utils/config.util';
-
-// Initialize knex.
-const knex = Knex(CONFIG.postgres);
-// Bind all Models to a knex instance. If you only have one database in
-// your server this is all you have to do. For multi database systems, see
-// the Model.bindKnex method.
-Model.knex(knex);
 
 import {stateMiddleware} from './middlewares/state.middleware';
 import loginRoutes from './routes/login.routes';
@@ -38,7 +29,9 @@ import oAuthRoutes from './routes/oauth.routes';
 import casRoutes from './routes/cas.routes';
 import userinfoRoutes from './routes/userinfo.routes';
 import infoRoutes from './routes/info.routes';
+import initDatabase from './models/database';
 
+const {knex} = initDatabase();
 const app = express();
 initPassport(app);
 app.oauth = new OAuthServer({
---
+++ src/models/database.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Configure and and update database
+ */
+
+import Knex from 'knex';
+import {Model} from 'objection';
+import {log} from '../utils/logging.util';
+import {CONFIG} from '../utils/config.util';
+
+/**
+ * Initialises database and bind knex instanse to models.
+ *
+ * Also runs latest migrations.
+ *
+ * @export
+ * @returns {knex: KnexInstance}
+ */
+export default function initialize() {
+  // Initialize knex.
+  const knex = Knex(CONFIG.postgres);
+
+  knex.migrate
+    .latest()
+    .then(() => {
+      log.info('Database is now at latest version.');
+    })
+    .catch(e => {
+      log.error('Could not update database to latest version', {
+        error: e.message,
+        stack: e.stack
+      });
+    });
+
+  // Bind all Models to a knex instance. If you only have one database in
+  // your server this is all you have to do. For multi database systems, see
+  // the Model.bindKnex method.
+  Model.knex(knex);
+
+  return {knex};
+}
