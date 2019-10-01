3ecea0f86b856754d93f90880eb696a82a488abe
mtni <mtni@dbc.dk>
2019-10-01 15:34:16 +0200

    solved lint err

--- src/client/libraryNameMap.js
+++ src/client/libraryNameMap.js
@@ -1,68 +1,65 @@
 export const libMap = {
   folk: {
-    '785100': ['Ålborg'],
-    '775100': ['Århus'],
-    '737000': ['Næver'],
-    '714700': ['Frb'],
-    '710100': ['Kbh'],
-    '758000': ['Åbenrå', 'Åbenraa', 'Aabenrå'],
-    '743000': ['Fåborg-Midtfyn'],
-    '716900': ['Høje-Tåstrup'],
-    '717300': ['Lyngby-Tårbæk', 'Lyngby-Taarbaek', 'Lyngby-Tårbaek'],
-    '718500': ['Taarnby']
+    785100: ['Ålborg'],
+    775100: ['Århus'],
+    737000: ['Næver'],
+    714700: ['Frb'],
+    710100: ['Kbh'],
+    758000: ['Åbenrå', 'Åbenraa', 'Aabenrå'],
+    743000: ['Fåborg-Midtfyn'],
+    716900: ['Høje-Tåstrup'],
+    717300: ['Lyngby-Tårbæk', 'Lyngby-Taarbaek', 'Lyngby-Tårbaek'],
+    718500: ['Taarnby']
   },
   forsk: {
-    '820050': ['Ålborg Universitetsbibliotek'],
-    '851510': ['Ålborg Universitetshospital. Medicinsk Bibliotek'],
-    '873870': ['Ålborg Zoo, Biblioteket'],
-    '875460': ['Biblioteket, Ålborg Tekniske Gymnasium'],
-    '874280': ['UU-Ålborg. Ungdommens Uddannelsesvejledning, Biblioteket'],
-    '874060': ['Århus Gymnasium og HF, Biblioteket'],
-    '875130': ['Århus Handelsgymnasium'],
-    '875290': ['Århus HF & VUC'],
-    '875020': ['Århus Maskinmesterskole. Videncenteret'],
-    '874570': ['Århus Statsgymnasium, Biblioteket'],
-    '874620': ['ARoS Århus Kunstmuseum, Biblioteket'],
-    '820080': ['Biblioteket Arkitektskolen Århus'],
-    '861160': ['Erhvervsakademi Århus, biblioteket'],
-    '861330': ['Jordbrugets UddannelsesCenter Århus. Biblioteket'],
-    '861800': ['SOSU Århus'],
-    '800010': ['Det Kongelige Bibliotek'],
-    '820010': [
+    820050: ['Ålborg Universitetsbibliotek'],
+    851510: ['Ålborg Universitetshospital. Medicinsk Bibliotek'],
+    873870: ['Ålborg Zoo, Biblioteket'],
+    875460: ['Biblioteket, Ålborg Tekniske Gymnasium'],
+    874280: ['UU-Ålborg. Ungdommens Uddannelsesvejledning, Biblioteket'],
+    874060: ['Århus Gymnasium og HF, Biblioteket'],
+    875130: ['Århus Handelsgymnasium'],
+    875290: ['Århus HF & VUC'],
+    875020: ['Århus Maskinmesterskole. Videncenteret'],
+    874570: ['Århus Statsgymnasium, Biblioteket'],
+    874620: ['ARoS Århus Kunstmuseum, Biblioteket'],
+    820080: ['Biblioteket Arkitektskolen Århus'],
+    861160: ['Erhvervsakademi Århus, biblioteket'],
+    861330: ['Jordbrugets UddannelsesCenter Århus. Biblioteket'],
+    861800: ['SOSU Århus'],
+    800010: ['Det Kongelige Bibliotek'],
+    820010: [
       'Det Kongelige Bibliotek - Aarhus',
       'Det Kongelige Bibliotek - Århus',
       'Det Kgl. Bibliotek - Århus'
     ],
-    '850010': ['Det Kongelige Bibliotek - Det Administrative Bibliotek'],
-    '810010': [
-      'Det Kongelige Bibliotek - København',
-      'Det Kgl. Bibliotek - Kbh'
-    ],
-    '830520': ['Det Kongelige Danske Musikkonservatorium, Biblioteket'],
-    '875650': ['Frb museerne'],
-    '840350': [
+    850010: ['Det Kongelige Bibliotek - Det Administrative Bibliotek'],
+    810010: ['Det Kongelige Bibliotek - København', 'Det Kgl. Bibliotek - Kbh'],
+    830520: ['Det Kongelige Danske Musikkonservatorium, Biblioteket'],
+    875650: ['Frb museerne'],
+    840350: [
       'Biblioteket for Etnologi, Forhistorisk og Klassisk Arkæologisk. Kbh Universitet, SAXO-Instituttet'
     ],
-    '875040': ['Center for Undervisningsmidler, Kbh'],
-    '875450': ['Handelsskolen Kbh Nord. Bibliotekerne'],
-    '875880': ['Institutbiblioteker ved Kbh Universitet (med begrænset udlån)'],
-    '876070': [
+    875040: ['Center for Undervisningsmidler, Kbh'],
+    875450: ['Handelsskolen Kbh Nord. Bibliotekerne'],
+    875880: ['Institutbiblioteker ved Kbh Universitet (med begrænset udlån)'],
+    876070: [
       'Institutbiblioteker ved Kbh Universitet (udlånes ikke interurbant)'
     ],
-    '820190': ['IT-Universitetet i Kbh, Biblioteket 2E09'],
-    '860900': ['Kbhs Erhvervsakademi Bibliotek'],
-    '830060': ['Kbhs Professionshøjskoles Bibliotek'],
-    '875400': ['Kbhs Stadsarkiv. Håndbogsbiblioteket'],
-    '851470': ['Kbhs Universitet. Det Humanistiske Fakultet. Polarbiblioteket'],
-    '875210': ['Kbhs VUC. Biblioteket'],
-    '860980': ['NEXT Uddannelse Kbh'],
-    '861150': [
+    820190: ['IT-Universitetet i Kbh, Biblioteket 2E09'],
+    860900: ['Kbhs Erhvervsakademi Bibliotek'],
+    830060: ['Kbhs Professionshøjskoles Bibliotek'],
+    875400: ['Kbhs Stadsarkiv. Håndbogsbiblioteket'],
+    851470: ['Kbhs Universitet. Det Humanistiske Fakultet. Polarbiblioteket'],
+    875210: ['Kbhs VUC. Biblioteket'],
+    860980: ['NEXT Uddannelse Kbh'],
+    861150: [
       'SOPU. Sundhed, omsorg, pædagosik. Kbh & Nordsjælland. Biblioteket'
     ],
-    '874400': ['Fåborg Gymnasium, Biblioteket'],
-    '872200': ['Høje-Tåstrup Gymnasium, Biblioteket'],
-    '874460': ['Borupgård Gymnasium, Studiecenteret'],
-    '875610': ['Skovgård Museet'],
-    '873630': ['Grenå Gymnasium og HF, Biblioteket']
+    874400: ['Fåborg Gymnasium, Biblioteket'],
+    872200: ['Høje-Tåstrup Gymnasium, Biblioteket'],
+    874460: ['Borupgård Gymnasium, Studiecenteret'],
+    875610: ['Skovgård Museet'],
+    873630: ['Grenå Gymnasium og HF, Biblioteket']
   }
 };
