5cb6200b88aa45f72043ab5e3282450327764c71
Finn Stausgaard <fvs@dbc.dk>
2022-06-28 08:11:26 +0200

    HEJMDAL-724 Use idp as fors

--- src/components/Forsrights/forsrights.client.js
+++ src/components/Forsrights/forsrights.client.js
@@ -3,37 +3,10 @@
  * Client for communicating with the forsrights service
  */
 
-import {CONFIG} from '../../utils/config.util';
 import {promiseRequest} from '../../utils/request.util';
+import {getDbcidpAgencyRights, validateIdpUser} from '../DBCIDP/dbcidp.client';
 import {log} from '../../utils/logging.util';
 import _ from 'lodash';
-
-/**
- * Function to prettify the forsrights response
- *
- * @param {array} forsrights response
- * @return {array} (prettified)
- */
-function filterForsrightsResponse(forsrights) {
-  // Map every agency in response
-  return forsrights.map(a => {
-    const a_rights = {};
-    if (a.agencyId) {
-      // Map every service in agency
-      if (Array.isArray(a.rights)) {
-        a.rights.forEach(s => {
-          const name = s.name.$;
-          // Map every rights in service + sort ascending
-          const s_rights = s.right.map(r => r.$).sort((an, bn) => an - bn);
-          // Set new servicename in object
-          a_rights[name] = s_rights;
-        });
-      }
-      return {agencyId: a.agencyId, rights: a_rights};
-    }
-    return {};
-  });
-}
 
 /**
  * Function to retrieve agency rights from forsrights service
@@ -78,33 +51,23 @@ export async function fetchRights(params) {
  * Function to retrieve (MULTIPLE) agency rights from forsrights service
  *
  * @param {string} accessToken
- * @param {array} agencies
+ * @param {object} user information
  * @return {array}
  */
 
-export async function getAgencyRights(accessToken, agencies) {
-  const requestUrl = CONFIG.forsrights.forsrightsUri;
+export async function getAgencyRights(accessToken, user) {
+  const dbcidp = await getDbcidpAgencyRights(accessToken, user);
 
-  const promises = agencies.map(agency => {
-    const requestParams = {
-      uri: requestUrl,
-      qs: {
-        action: 'forsRights',
-        outputType: 'json',
-        accessToken,
-        agency
-      }
-    };
-
-    return fetchRights(requestParams);
+  const fakeFors = [];
+  dbcidp.forEach((agency) => {
+    const fors = { agencyId: agency.agencyId, rights: {} };
+    agency.rights.forEach((product) => {
+      fors.rights[product.productName.toLowerCase()] = ['500', '501'];
+    })
+    fakeFors.push(fors);
   });
 
-  const result = await Promise.all(promises);
-
-  // Filter the forsrights response before return
-  const filteredResult = filterForsrightsResponse(result);
-
-  return filteredResult;
+  return fakeFors;
 }
 
 /**
@@ -118,34 +81,5 @@ export async function getAgencyRights(accessToken, agencies) {
  */
 
 export async function validateNetpunktUser(userIdAut, groupIdAut, passwordAut) {
-  let requestUrl = CONFIG.forsrights.forsrightsUri;
-
-  const requestParams = {
-    uri: requestUrl,
-    qs: {
-      action: 'forsRights',
-      userIdAut,
-      groupIdAut,
-      passwordAut,
-      outputType: 'json'
-    }
-  };
-
-  try {
-    const response = await promiseRequest('get', requestParams);
-    const parsedBody = JSON.parse(response.body);
-
-    /**
-     * Validation returns true if no 'error' object is found in the response.body
-     * - No further validation is done
-     */
-    const isValid = !_.get(parsedBody, 'forsRightsResponse.error', false);
-    return isValid;
-  } catch (error) {
-    log.error('Error validating netpunkt-triple-user', {
-      error: error.message,
-      stack: error.stack
-    });
-    return false;
-  }
+  return await validateIdpUser(userIdAut, groupIdAut, passwordAut);
 }
