843061c6406d379acef5b448a122b8c523e93aa4
Finn Stausgaard <fvs@dbc.dk>
2022-11-02 11:09:14 +0100

    Hejmdal 730: Set defaults for blocked and userPrivilege

--- src/components/Culr/culr.component.js
+++ src/components/Culr/culr.component.js
@@ -149,7 +149,7 @@ export async function getBorchkInfo(user) {
  * @returns {Promise<{}>}
  */
 export async function getUserInfoFromBorchk(culrResponse, user) {
-  let response = {};
+  let response = {blocked: false, userPrivilege: []};
 
   try {
     // check if user lives in municipality
