0cb73550551f0ec99c0cb5d851082d3a2b9e7b13
Finn Stausgaard <fvs@dbc.dk>
2022-11-16 12:55:54 +0100

    Revert "Hejmdal 748 wrong municipality agency (#697)"
    This reverts commit 14d1d2423f4bc0cd927240f5e80eb4e5cf933687.

--- e2e/cypress/integration/login-with-borchk.js
+++ e2e/cypress/integration/login-with-borchk.js
@@ -136,9 +136,8 @@ context('Login flow', () => {
   });
 
   it('Should return work-municipality for librarians working outside their home-municipality', () => {
-    const municipalityWork = 615;
+    const municipalityWork = 370;
     const municipalityAgency = 761500;
-
     const userId = '0102030410';
 
     cy.visit(authorize(`${municipalityAgency}`));
