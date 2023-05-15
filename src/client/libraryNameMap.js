88cd0f463e982b3d6da4fb94e368d47845b459a7
Finn Stausgaard <fvs@dbc.dk>
2023-05-15 09:48:56 +0200

    Adg 785 alternate name for 911130 greenland (#733)
    * ADG-785 Change auth name for 911130 and use old name as alternate search form

--- src/client/libraryNameMap.js
+++ src/client/libraryNameMap.js
@@ -11,7 +11,7 @@ export const libMap = {
     717300: ['Lyngby-Tårbæk', 'Lyngby-Taarbaek', 'Lyngby-Tårbaek'],
     718500: ['Taarnby'],
     911116: ['Tórshavn', 'Torshavn', 'Thorshavn'],
-    911130: ['Kalaallit Nunaat', 'Kalaallit', 'Nunaat']
+    911130: ['Nunatta Atuagaateqarfia','Nunatta', 'Atuagaateqarfia']
   },
   forsk: {
     820050: ['Ålborg Universitetsbibliotek'],
--- src/utils/municipality.util.js
+++ src/utils/municipality.util.js
@@ -8,7 +8,7 @@ export const municipalityName = {
   700400: 'Sydslesvig',
   710100: 'København',
   911116: 'Býarbókasavnið',
-  911130: 'Nunatta Atuagaateqarfia',
+  911130: 'Kalaallit Nunaat',
   714700: 'Frederiksberg',
   715100: 'Ballerup',
   715300: 'Brøndby',
