512593227a48dae3219786c776c269b5e54db92f
Finn Stausgaard <fvs@dbc.dk>
2024-03-14 13:35:16 +0100

    bump objection and cypress

---
+++ e2e/cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  video: false,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: '/app/e2e/reports/test-result-[hash].xml',
+    toConsole: true,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3011',
+    supportFile: false,
+    specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
+  },
+})
