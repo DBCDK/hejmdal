bb1b727af19f90e436260cbba01ffabf3fc96663
Finn Stausgaard <fvs@dbc.dk>
2024-03-18 14:57:09 +0100

    bump objection and cypress - comment 2 tests

--- e2e/cypress/e2e/borchk-form.cy.js
+++ e2e/cypress/e2e/borchk-form.cy.js
@@ -225,9 +225,6 @@ context('Borchk form', () => {
     for (var i = 2; i; i--) {
       cy.get('#userid-input').type(uid);
       cy.get('[data-cy=pin-input]').type('1234{enter}');
-      const xx = '#error-body';
-      cy.task('log','error-body txt ');
-      cy.task('log', xx);
       cy.get('#error-body').debug();
       cy.get('#error-body').should(
         'contain',
